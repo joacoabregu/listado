a4f2157c2128712ac59449cf730c8f8f6d92257c
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-19 17:49:22 -0300

    config inicial

---
+++ src/pages/Addresses.js
@@ -0,0 +1,55 @@
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function Addresses() {
+    let [addresses, setAddresses] = useState([]);
+
+    useEffect(() =>{
+       axios.get('https://fakerapi.it/api/v1/addresses')
+        .then((response) => {
+            console.log(response.data);
+            setAddresses(response.data.data);
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+    }, [])
+    if(!addresses.length){
+        return <h1>Searching...</h1>       
+    } else{
+        return(
+            <table >
+                <thead>
+                <tr>
+                    <th >Street</th>
+                    <th >Street Name</th>
+                    <th >Number</th>
+                    <th >City</th>
+                    <th >Zipcode</th>
+                    <th >Country</th>
+                </tr>
+                </thead>
+                <tbody>
+                {addresses.map((address, index) => {
+                    return (
+                        <tr key={index} >
+                            <td >{address.street}</td>
+                            <td >{address.streetName}</td>
+                            <td >{address.buildingNumber}</td>
+                            <td >{address.city}</td>
+                            <td >{address.zipcode}</td>
+
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+            
+        )
+    }
+    
+}
+
+export default Addresses;
