d5eb1cade06d050e6f8627a85ad3711433c0382b
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-19 20:12:55 -0300

    initial config

---
+++ src/pages/Products.js
@@ -0,0 +1,52 @@
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function Products() {
+    let [products, setProducts] = useState([]);
+
+    useEffect(() =>{
+       axios.get('https://fakerapi.it/api/v1/products')
+        .then((response) => {
+            console.log(response.data);
+            setProducts(response.data.data);
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+    }, [])
+    if(!products.length){
+        return <h1>Searching...</h1>       
+    } else{
+        return(
+            <table >
+                <thead>
+                <tr>
+                    <th >Name</th>
+                    <th >Description</th>
+                    <th >Price</th>
+                    <th >Image</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map((product, index) => {
+                    return (
+                        <tr key={index} >
+                            <td >{product.name}</td>
+                            <td >{product.description}</td>
+                            <td >{product.price}</td>
+                            <td><img alt="cover" src={product.image} /></td>  
+
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+            
+        )
+    }
+    
+}
+
+export default Products;
