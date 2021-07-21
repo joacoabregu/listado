05ab6411ebc3e7672b15f1947819588bf2489e3c
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-20 21:33:34 -0300

    initial config

---
+++ src/components/Pagination.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Pagination = ({ perPage, total, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+    pageNumbers.push(i);
+  }
+  
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={(e) => 
+               { e.preventDefault()
+                paginate(number)}
+              } href='!#' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
